2d9d1d820246544a53dd43339c3d3d141186c7fb
D3Brooksster <137219076+D3Brooksster@users.noreply.github.com>
2025-05-30 22:08:27 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import cron from 'node-cron';
 import { deleteInactiveAccounts } from './jobs/accountCleanupJob';
 
 const app = express();
+app.set('trust proxy', true);
 
 app.use(express.json());
 
